b7df152d54e67c927183ec4873cc7c6222512069
Matt Mahowald <mcm2018@stanford.edu>
2015-10-08 02:28:57 -0700

    Updated crypto.py

--- crypto.py
+++ crypto.py
@@ -2,24 +2,28 @@
 File: crypto.py
 Name: Matt Mahowald
 Date: October 9th 2015
+Description: crypto.py implements a cryptography suite that allows
+the user to input a message or a filename and then either encrypt 
+or decrypt the message
 """
+
+NUM_ALPHA_LETTERS = 26
 
 def encrypt_caesar(plaintext):
     """
     Encrypts plaintext using a Caesar cipher.
     Add more implementation details here.
     """
+    caesar_offset = 3
     new_string = ""
     uppertext = plaintext.upper()
     for letter in uppertext:
         if(letter.isalpha()):
-            if(chr((ord(letter) + 4)).isalpha()):
-                new_string += chr((ord(letter) + 3))
+            if((ord(letter) + caesar_offset) <= ord('Z')):
+                new_string += chr((ord(letter) + caesar_offset))
             else:
-                new_string += chr((ord(letter) - 23))
+                new_string += chr((ord(letter) - NUM_ALPHA_LETTERS + caesar_offset))
     return new_string
-
-
 
 def decrypt_caesar(ciphertext):
     """
@@ -30,32 +34,63 @@ def decrypt_caesar(ciphertext):
     uppertext = ciphertext.upper()
     for letter in uppertext:
         if(letter.isalpha()):
-            new_string += chr((ord(letter) - 4))
+            if((ord(letter) + 3) < ord('Z')):
+                new_string += chr((ord(letter) + 3))
+            else:
+                new_string += chr((ord(letter) - 23))
     return new_string
-
 
 def encrypt_vigenere(plaintext, keyword):
     """
     Encrypts plaintext using a Vigenere cipher with a keyword.
     Add more implementation details here.
     """
-    pass  # Your implementation here
-
+    new_string = ""
+    for i in range(len(plaintext)):
+        key_char = keyword[i % len(keyword)]
+        new_chr = chr(ord(plaintext[i]) + ord(key_char) - ord('A'))
+        if(not new_chr.isalpha()):
+            new_chr = chr(ord(new_chr) - 26)
+        new_string += new_chr
+    return new_string
 
 def decrypt_vigenere(ciphertext, keyword):
     """
     Decrypts ciphertext using a Vigenere cipher with a keyword.
     Add more implementation details here.
     """
-    pass  # Your implementation here
-
-
+    new_string = ""
+    for i in range(len(ciphertext)):
+        key_char = keyword[i % len(keyword)]
+        new_chr = chr(ord(ciphertext[i]) + ord('A') - ord(key_char))
+        if(not new_chr.isalpha()):
+            new_chr = chr(ord(new_chr) + 26)
+        new_string += new_chr
+    return new_string
+    
 def encrypt_railfence(plaintext, num_rails):
     """
     Encrypts plaintext using a railfence cipher.
     Add more implementation details here.
     """
-    pass  # Your implementation here
+    rails = ["" for i in range(num_rails)]
+    print(rails)
+   
+    row = 0
+    d = 1
+    for ch in plaintext:
+        if(not ch.isalpha()): continue
+        rails[row] = rails[row] + ch
+        row += d
+        if row == num_rails:
+           d = -1
+           row -= 2
+        if row == -1:
+           d = 1
+           row += 2 
+    print(rails)
+    return "".join(rails)
+
 
 def decrypt_railfence(ciphertext, num_rails):
     """
@@ -64,14 +99,12 @@ def decrypt_railfence(ciphertext, num_rails):
     """
     pass  # Your implementation here
 
-
 def read_from_file(filename):
     """
     Reads and returns content from a file.
     Add more implementation details here.
     """
     pass  # Your implementation here
-
 
 def write_to_file(filename, content):
     """
@@ -80,24 +113,34 @@ def write_to_file(filename, content):
     """
     pass  # Your implementation here
 
+
+def get_keyword():
+    return input("Enter a keyword: ").upper()
+
+def get_nrails():
+    return int(input("Enter an integer: "))
+
 def transform_text(raw_text, operation, tool):
     print("*Output*") 
     if(operation == 'E'):
         if(tool == 'C'):
             return encrypt_caesar(raw_text)
         elif(tool == 'V'):
-            return "Not Implemented!"
+            return encrypt_vigenere(raw_text, get_keyword())
         elif(tool == 'R'):
-            return "Not Implemented!"
+            return encrypt_railfence(raw_text, get_nrails())
     elif(operation == 'D'):
         if(tool == 'C'):
             return decrypt_caesar(raw_text)
         elif(tool == 'V'):
-            return "Not Implemented!"
+            return decrypt_vigenere(raw_text, get_keyword())
         elif(tool == 'R'):
-            return "Not Implemented!"
+            return encrypt_railfence(raw_text, get_nrails())
     else:
         print("We're sorry, an error has occurred")
+
+def prompt_for_filename():
+    return input('Please enter a valid filename: ')
 
 def run_suite():
     """
@@ -112,12 +155,11 @@ def run_suite():
         filename = prompt_for_filename()
         raw_text = read_from_file(filename)
     else:
-        raw_text = input("Enter the string to encrypt or decrypt: ")
+        raw_text = input("Enter the string to encrypt or decrypt: ").upper()
     operation = transformation_type()
     tool = encryption_method() 
     message = transform_text(raw_text, operation, tool)
     print(message)
-
 
 def encryption_method():
     input_type = input("(C)aesar, (V)igenere, or (R)ailfence? ").upper()
